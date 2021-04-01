a428ed391637e516ed1d0b20224dd02d224d0290
Grana69 <siscom.grana@gmail.com>
2021-04-01 15:45:52 -0700

    added gitignore & trycatch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
-import { CREATE_USER_MUTATION } from '../GraphQL/Mutation';
-import { useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import { CREATE_USER_MUTATION } from "../GraphQL/Mutation";
+import { useMutation } from "@apollo/client";
 
 function Form() {
     const [firstName, setFirstName] = useState("");
@@ -8,17 +8,22 @@ function Form() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const [createUser, {error}] = useMutation(CREATE_USER_MUTATION);
+    const [createUser, { error }] = useMutation(CREATE_USER_MUTATION);
 
-    const addUser = () => {
-        createUser({
-            variables: {
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password
-            }
-        })
+    const addUser = async () => {
+        try {
+            const data = await createUser({
+                variables: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password
+                },
+            });
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
         if (error) {
             console.log(error);
         }
@@ -26,13 +31,37 @@ function Form() {
 
     return (
         <div>
-            <input type="text" placeholder="First Name" onChange={(e) => {setFirstName(e.target.value)}} />
-            <input type="text" placeholder="Last Name" onChange={(e) => {setLastName(e.target.value)}} />
-            <input type="text" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} />
-            <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} />
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={(e) => {
+                    setFirstName(e.target.value);
+                }}
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) => {
+                    setLastName(e.target.value);
+                }}
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                }}
+            />
+            <input
+                type="text"
+                placeholder="Password"
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                }}
+            />
             <button onClick={addUser}>Create User</button>
         </div>
-    )
+    );
 }
 
-export default Form
+export default Form;
